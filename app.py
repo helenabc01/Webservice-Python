40e606aeb5008900dae8acd4aecedc3584fd3c2a
Helena <helena.costa@germinare.org.br>
2023-06-13 17:08:36 -0300

    enviando

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask;
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<center><h1>BEM VINDO AO MUNDO... DEVOPS.!!! -- Version: 0.3</h1></center>'
+
+@app.route('/msg')
+def msg():
+    return '<center><h1>Usando os conceitos de deploy..!!!</h1></center>'
+
+app.run();
